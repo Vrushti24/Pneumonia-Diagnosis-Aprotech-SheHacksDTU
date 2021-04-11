f886876e6aed30ed96d95929752709d8b49c699d
Sejal Khanna <67584092+sejalllkhanna@users.noreply.github.com>
2021-04-11 10:40:23 +0530

    Add files via upload

---
+++ DoctorViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DoctorViewController.swift
+//  PneumoniaDiagnosis
+//
+//  Created by Mac on 10/04/21.
+//  Copyright © 2021 Sejal Khanna. All rights reserved.
+//
+
+import UIKit
+
+//func viewDidLoad() {
+//    super.viewDidLoad()
+//
+//
+//}
---
+++ Information View Controller.swift
@@ -0,0 +1,9 @@
+//
+//  InformationViewController.swift
+//  PneumoniaDiagnosis
+//
+//  Created by Mac on 11/04/21.
+//  Copyright © 2021 Sejal Khanna. All rights reserved.
+//
+
+import Foundation
